83dafec28c5248b55396e4205d286d499b16d3f8
inkyu <gofiri@gmail.com>
2016-09-19 02:19:15 +0800

    implement ui thread

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 extern crate libc;
 mod io;
+mod ui;
+
+use ui::event::Event;
+use std::str::FromStr;
 
 fn main() {
     io::input::nonblock_init().unwrap();
     let mut ev = io::event::Event::new().unwrap();
+    let chan = ui::handler::launch();
     ev.init().unwrap();
-    ev.handle(|e| {
-        let mut buf = Vec::with_capacity(256);;
-        println!("{}", e.ident);
-        try!(io::input::read(&mut buf).map_err(|e| e.to_string()));
-        let s = try!(String::from_utf8(buf).map_err(|e| e.to_string()));
-        println!("{}", s);
-        Ok(())
-    }).unwrap();
+    loop {
+        ev.handle(|e| {
+            let mut buf = Vec::with_capacity(256);;
+            println!("{}", e.ident);
+            try!(io::input::read(&mut buf).map_err(|e| e.to_string()));
+            let s = try!(String::from_utf8(buf).map_err(|e| e.to_string()));
+            let e = try!(Event::from_str(&s).map_err(|e| e.to_string()));
+            try!(chan.send(e).map_err(|e| e.to_string()));
+            Ok(())
+        }).unwrap();
+    }
 }
