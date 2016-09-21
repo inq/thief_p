431f10674db9f6cb65cdd24c12819ce21711f28d
inkyu <gofiri@gmail.com>
2016-09-22 02:23:23 +0800

    pass string to the ui-thread

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,3 +1,13 @@
-pub mod event;
-pub mod input;
-pub mod term;
+use std::error::{Error};
+
+mod event;
+mod term;
+mod input;
+
+pub use io::event::{Event};
+
+pub fn init() -> Result<(), Box<Error>> {
+    try!(input::init());
+    try!(term::init());
+    Ok(())
+}
--- src/ui/handler.rs
+++ src/ui/handler.rs
@@ -1,14 +1,13 @@
 use std::thread;
 use std::sync::mpsc::{self, channel};
-use ui::event::Event;
 
-pub fn launch() -> mpsc::Sender<Event> {
+pub fn launch() -> mpsc::Sender<String> {
     let (tx, rx) = channel();
     thread::spawn(move || {
         loop {
-            let e = rx.recv().unwrap();
-            println!("{:?}", e);
-        }
+            let s = rx.recv().unwrap();
+            println!("{}", s);
+        };
     });
     tx
 }
