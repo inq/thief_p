75552900f1f65fd9a52b4a1fdb0b1ee4958f1631
inkyu <gofiri@gmail.com>
2016-09-23 01:00:12 +0800

    parse move & meta events

--- src/ui/event.rs
+++ src/ui/event.rs
@@ -1,37 +1,43 @@
-use std::{error, fmt, str};
-
 #[derive(Debug)]
 pub enum Event {
-    Char{ c: char }
+    Char{ c: char },
+    Move{ x: i8, y: i8 },
+    Meta{ c: char },
 }
 
-impl str::FromStr for Event {
-    type Err = Error;
-
-    fn from_str(s: &str) -> Result<Event, Error> {
-        match s {
-            "c" => Ok(Event::Char { c: 'c' }),
-            _ => Err(Error::Parse),
+impl Event {
+    pub fn from_string(s: String) -> (Option<Event>, String) {
+        let mut it = s.chars();
+        match it.next() {
+            Some('\u{1b}') => from_escape(&s),
+            Some(c) => (Some(Event::Char { c : c }), it.collect()),
+            None => (None, it.collect())
         }
     }
 }
 
-
-#[derive(Debug)]
-pub enum Error {
-    Parse,
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        error::Error::description(self).fmt(f)
+fn check_prefix(s: &String, t: &str) -> Option<String> {
+    if s.starts_with(&t) {
+        Some(s.chars().skip(t.len()).collect())
+    } else {
+        None
     }
 }
 
-impl error::Error for Error {
-    fn description(&self) -> &str {
-        match *self {
-            Error::Parse => "parse error"
+fn from_escape(s: &String) -> (Option<Event>, String) {
+    if let Some(r) = check_prefix(&s, "\u{1b}[A") {
+        (Some(Event::Move{ x: 0, y: -1 }), r)
+    } else if let Some(r) = check_prefix(&s, "\u{1b}[B") {
+        (Some(Event::Move{ x: 0, y: 1 }), r)
+    } else if let Some(r) = check_prefix(&s, "\u{1b}[C") {
+        (Some(Event::Move{ x: 1, y: 0 }), r)
+    } else if let Some(r) = check_prefix(&s, "\u{1b}[D") {
+        (Some(Event::Move{ x: -1, y: 0 }), r)
+    } else {
+        let mut it = s.chars().skip(1);
+        match it.next() {
+            Some(k) => (Some(Event::Meta{ c: k }), it.collect()),
+            None => (None, it.collect())
         }
     }
 }
