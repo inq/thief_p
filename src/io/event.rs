e52a1f7ff9e466c0d38e65cee68ec1765f90c58a
inkyu <gofiri@gmail.com>
2016-09-23 18:26:40 +0800

    format code

--- src/io/event.rs
+++ src/io/event.rs
@@ -1,8 +1,8 @@
 #[derive(Debug)]
 pub enum Event {
-    Char{ c: char },
-    Move{ x: i8, y: i8 },
-    Meta{ c: char },
+    Char { c: char },
+    Move { x: i8, y: i8 },
+    Meta { c: char },
 }
 
 impl Event {
@@ -10,8 +10,8 @@ impl Event {
         let mut it = s.chars();
         match it.next() {
             Some('\u{1b}') => from_escape(&s),
-            Some(c) => (Some(Event::Char { c : c }), it.collect()),
-            None => (None, it.collect())
+            Some(c) => (Some(Event::Char { c: c }), it.collect()),
+            None => (None, it.collect()),
         }
     }
 }
@@ -26,18 +26,18 @@ fn check_prefix(s: &String, t: &str) -> Option<String> {
 
 fn from_escape(s: &String) -> (Option<Event>, String) {
     if let Some(r) = check_prefix(&s, "\u{1b}[A") {
-        (Some(Event::Move{ x: 0, y: -1 }), r)
+        (Some(Event::Move { x: 0, y: -1 }), r)
     } else if let Some(r) = check_prefix(&s, "\u{1b}[B") {
-        (Some(Event::Move{ x: 0, y: 1 }), r)
+        (Some(Event::Move { x: 0, y: 1 }), r)
     } else if let Some(r) = check_prefix(&s, "\u{1b}[C") {
-        (Some(Event::Move{ x: 1, y: 0 }), r)
+        (Some(Event::Move { x: 1, y: 0 }), r)
     } else if let Some(r) = check_prefix(&s, "\u{1b}[D") {
-        (Some(Event::Move{ x: -1, y: 0 }), r)
+        (Some(Event::Move { x: -1, y: 0 }), r)
     } else {
         let mut it = s.chars().skip(1);
         match it.next() {
-            Some(k) => (Some(Event::Meta{ c: k }), it.collect()),
-            None => (None, it.collect())
+            Some(k) => (Some(Event::Meta { c: k }), it.collect()),
+            None => (None, it.collect()),
         }
     }
 }
