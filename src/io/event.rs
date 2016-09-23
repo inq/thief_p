3a101ae40ce1fef9d6d0a0fac8a58e6dde36dc37
inkyu <gofiri@gmail.com>
2016-09-24 04:20:19 +0800

    process ctrl key

--- src/io/event.rs
+++ src/io/event.rs
@@ -1,16 +1,28 @@
+use std::char;
+
 #[derive(Debug)]
 pub enum Event {
     Char { c: char },
+    Ctrl { c: char },
     Move { x: i8, y: i8 },
     Meta { c: char },
+    Escape,
 }
 
 impl Event {
+    pub fn from_char(c: char) -> Event {
+        if c as u32 <= 26 {
+            Event::Ctrl { c: (c as u8 + 'a' as u8 - 1) as char }
+        } else {
+            Event::Char { c: c }
+        }
+    }
+
     pub fn from_string(s: String) -> (Option<Event>, String) {
         let mut it = s.chars();
         match it.next() {
             Some('\u{1b}') => from_escape(&s),
-            Some(c) => (Some(Event::Char { c: c }), it.collect()),
+            Some(c) => (Some(Event::from_char(c)), it.collect()),
             None => (None, it.collect()),
         }
     }
@@ -37,7 +49,7 @@ fn from_escape(s: &String) -> (Option<Event>, String) {
         let mut it = s.chars().skip(1);
         match it.next() {
             Some(k) => (Some(Event::Meta { c: k }), it.collect()),
-            None => (None, it.collect()),
+            None => (Some(Event::Escape), it.collect()),
         }
     }
 }
