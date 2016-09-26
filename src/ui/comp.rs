fa29f961a34615eb27eea5943df714386cfeaa55
inkyu <gofiri@gmail.com>
2016-09-26 10:00:34 +0800

    implement response for components

--- src/ui/comp.rs
+++ src/ui/comp.rs
@@ -1,3 +1,5 @@
+use ui::prim::Buffer;
+
 pub trait Component {
     fn resize(&mut self, width: usize, height: usize);
 }
@@ -6,4 +8,14 @@ pub struct Child<T: Component> {
     pub comp: T,
     pub x: usize,
     pub y: usize,
+}
+
+pub struct Cursor {
+    pub x: usize,
+    pub y: usize,
+}
+
+pub struct Response {
+    pub draw: Option<Buffer>,
+    pub cursor: Option<Cursor>,
 }
