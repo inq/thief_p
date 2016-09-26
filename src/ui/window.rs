f6d763a8f56f50de4f7778e7a9ec652fba558145
inkyu <gofiri@gmail.com>
2016-09-26 08:12:28 +0800

    implement editor

--- src/ui/window.rs
+++ src/ui/window.rs
@@ -1,13 +1,19 @@
+use ui::editor::Editor;
 use ui::prim::{Buffer, Brush, Color};
 
 pub struct Window {
+    editor: (Editor, usize, usize),
     width: usize,
     height: usize,
 }
 
 impl Window {
     pub fn new(width: usize, height: usize) -> Window {
-        Window { width: width, height: height }
+        Window {
+            editor: (Editor::new(width, height), 1, 1),
+            width: width,
+            height: height
+        }
     }
 }
 
@@ -15,10 +21,13 @@ impl Window {
     pub fn resize(&mut self, width: usize, height: usize) {
         self.width = width;
         self.height = height;
+        self.editor.0.resize(width - 2, height - 2);
     }
 
     pub fn refresh(&self) -> Buffer {
         let b = Brush::new(Color::new(0, 0, 0), Color::new(200, 200, 200));
-        Buffer::bordered(&b, &b.invert(), self.width, self.height)
+        let mut buffer = Buffer::blank(&b, self.width, self.height);
+        buffer.draw(&self.editor.0.refresh(), 1, 1);
+        buffer
     }
 }
