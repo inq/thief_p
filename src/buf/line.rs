7670ae178722d8766ada2b5abe58fb380ec8a8ca
inkyu <gofiri@gmail.com>
2016-10-27 02:57:00 +0800

    append string; move cursor

--- src/buf/line.rs
+++ src/buf/line.rs
@@ -3,25 +3,76 @@ pub struct Line {
     after: String,
 }
 
+const BUFSIZE: usize = 80;
+
 impl Line {
     pub fn new() -> Line {
         Line {
-            before: String::with_capacity(80),
-            after: String::with_capacity(80),
+            before: String::with_capacity(BUFSIZE),
+            after: String::with_capacity(BUFSIZE),
         }
     }
 
+    /// Append string before cursor.
+    pub fn append_before_cursor(&mut self, str: &String) {
+        self.before.push_str(str);
+    }
+
+    /// Append string after cursor.
+    pub fn append_after_cursor(&mut self, str: &String) {
+        let reversed = str.chars().rev().collect::<String>();
+        self.after.push_str(&reversed);
+    }
+
+    /// Move cursor by 1 character.
+    /// If `right` is `true`, then move to right direction. Otherwise,
+    /// move to left direction. Returns `true` if succeed.
+    pub fn move_cursor(&mut self, right: bool) -> bool {
+        let (from, to) = if right {
+            (&mut self.after, &mut self.before)
+        } else {
+            (&mut self.before, &mut self.after)
+        };
+        match from.pop() {
+            Some(c) => {
+                to.push(c);
+                true
+            }
+            None => false
+        }
+    }
+
+    /// Convert to a string.
+    /// This can be used for the debugging purpose.
     pub fn to_string(&self) -> String {
-        let reversed = self.after.chars().rev().collect::<String>();
-        format!("{}{}", self.before, reversed)
+        let mut res = self.before.to_owned();
+        res.push_str(&self.after.chars().rev().collect::<String>());
+        res
     }
 }
 
 #[test]
 fn basic_operations() {
-    let t = Line {
-        before: String::from("hello"),
-        after: String::from("world"),
-    };
-    assert_eq!(t.to_string(), "hellodlrow");
+    let mut t = Line::new();
+    t.append_before_cursor(&String::from("hello"));
+    assert_eq!(t.to_string(), "hello");
+    t.append_after_cursor(&String::from("world"));
+    assert_eq!(t.to_string(), "helloworld");
+}
+
+#[test]
+fn move_cursor() {
+    let mut t = Line::new();
+    t.append_before_cursor(&String::from("hello"));
+    t.append_after_cursor(&String::from("world"));
+    for _ in 0..5 {
+        assert_eq!(t.move_cursor(true), true);
+    }
+    assert_eq!(t.move_cursor(true), false);
+    assert_eq!(t.after, "");
+    for _ in 0..10 {
+        assert_eq!(t.move_cursor(false), true);
+    }
+    assert_eq!(t.move_cursor(false), false);
+    assert_eq!(t.before, "");
 }
