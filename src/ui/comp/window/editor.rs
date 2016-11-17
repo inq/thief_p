d165a6df5237dbc98d6b59576efcb1d3f174b024
inkyu <gofiri@gmail.com>
2016-11-17 22:14:12 +0800

    break line by CR

--- src/ui/comp/window/editor.rs
+++ src/ui/comp/window/editor.rs
@@ -65,7 +65,10 @@ impl Component for Editor {
                 }
             }
             Event::Ctrl { c } => { // CR
-                Default::default()
+                self.buffer.break_line();
+                self.cursor.x = self.buffer.get_x();
+                self.cursor.y = self.buffer.get_y();
+                self.refresh()
             }
             Event::Char { c } => {
                 self.buffer.insert(c);
